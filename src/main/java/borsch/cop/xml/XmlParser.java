26e15c0019a66963bbf566d250bad552de24657f
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2017-09-18 22:54:31 +0300

    Add ignore file

---
+++ src/main/java/borsch/cop/xml/XmlParser.java
@@ -0,0 +1,10 @@
+package borsch.cop.xml;
+
+/**
+ * Created by Oleh on 18.09.2017.
+ */
+public class XmlParser {
+
+
+
+}
---
+++ src/main/java/borsch/tester/Practice1.java
@@ -0,0 +1,12 @@
+package borsch.tester;
+
+/**
+ * Created by Oleh on 18.09.2017.
+ */
+public class Practice1 {
+
+    public static void main(String[] args) {
+        System.out.println("asd");
+    }
+
+}
